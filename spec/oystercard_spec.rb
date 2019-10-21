dc55a34cda6657523187e62bedb4353f21e5dc1b
Ed Courage <edcourage@live.com>
2019-10-21 15:35:48 +0100

    chapter4 - complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  context 'when card is intialized,' do
 
+    it 'instance of card is expected to have default balance of 0' do
+      expect(oystercard.balance).to eq 0
+    end
+
+
+
+  end
 end
