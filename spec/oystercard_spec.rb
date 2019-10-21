02a4484e520e762a76114a8f588f5bcb8f6f5017
Natalie <49326857+natyeo@users.noreply.github.com>
2019-10-21 15:58:03 +0100

    Added topup method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,12 +3,16 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
   context 'when card is intialized,' do
-
     it 'instance of card is expected to have default balance of 0' do
       expect(oystercard.balance).to eq 0
     end
+  end
 
-
-
+  describe '#top_up' do
+    it 'does top_up add money to card' do
+      oystercard.top_up(10)
+      oystercard.top_up(5)
+      expect(oystercard.balance).to eq 15
+    end
   end
 end
