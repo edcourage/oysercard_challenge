b54fc22d417b53cb6174606c8f5080c1ee2d44e2
Natalie <49326857+natyeo@users.noreply.github.com>
2019-10-21 17:33:55 +0100

    Added touch in, touch out functionality and check card in use

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,6 +4,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up(amount)
@@ -15,10 +16,22 @@ class Oystercard
     @balance -= amount
   end
 
-  private
-
-  def maxed?(amount)
-    @balance + amount > CARD_LIMIT
+  def in_journey?
+    @in_use
   end
+
+ def touch_in
+   @in_use = true
+ end
+
+ def touch_out
+   @in_use = false
+ end
+
+ private
+
+ def maxed?(amount)
+   @balance + amount > CARD_LIMIT
+ end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,10 +6,14 @@ describe Oystercard do
     it 'instance of card is expected to have default balance of 0' do
       expect(oystercard.balance).to eq 0
     end
+
+    it 'is not in_journey' do
+      expect(oystercard).not_to be_in_journey
+    end
   end
 
   describe '#top_up' do
-    it 'does top_up add money to card' do
+    it 'adds money to card' do
       expect{ oystercard.top_up 15 }.to change{ oystercard.balance}.by 15
     end
 
@@ -20,9 +24,24 @@ describe Oystercard do
   end
 
   describe '#deduct' do
-    it 'does deduct take money off card' do
+    it 'takes money off card' do
       oystercard.top_up(10)
       expect{ oystercard.deduct 5 }.to change{ oystercard.balance}.by -5
     end
   end
+
+  describe '#touch_in' do
+    it 'registers card is in_journey?' do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+  end
+
+  describe '#touch_out' do
+    it 'registers card is no longer in_journey?' do
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
+  end
+
 end
